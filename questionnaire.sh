105ecde2486aeab2d69d281977a9154130de3acd
Joel J Aleman <joel.aleman7@hotmail.com>
2025-06-09 09:01:03 +0000

    Save progress: 260.1

--- questionnaire.sh
+++ questionnaire.sh
@@ -2,8 +2,7 @@
 QUESTION1="What's your name?"
 QUESTION2="Where are you from?"
 echo $QUESTION1
-NAME=var
 read NAME
 echo $QUESTION2
 read LOCATION
-echo Hello $NAME.
+echo Hello $NAME from $LOCATION.
